8d0487ce928ae9d1b4eacd3ef6f1f605438d6629
ucwillzhou <>
2021-08-07 00:47:22 -0500

    added nav links to admin page

--- nailstyle-system/src/admin/components/Navbar/Navbar.js
+++ nailstyle-system/src/admin/components/Navbar/Navbar.js
@@ -1,19 +1,41 @@
+import "./Navbar.css";
+import { BsFillCalendarFill, BsPeopleFill, BsGraphUp, BsFillPersonLinesFill} from "react-icons/bs";
 
-import "./Navbar.css"
 
 function Navbar() {
-    return (
-        <div className="navbar-container">
-            <div className="logo">
-                NAILSTYLE
+  return (
+    <div className="navbar-container">
+      <div className="logo">NAIL</div>
+      <div className="categories">
+        <ul className="links">
+          <li>
+            <div className="link">
+                <BsFillCalendarFill className="icon"></BsFillCalendarFill>
+                <div>Bookings</div>
             </div>
-            <div className="categories">
-                <a href="">Overview</a>
+          </li>
+          <li>
+            <div className="link">
+                <BsPeopleFill className="icon"></BsPeopleFill>
+                <div>Customers</div>
             </div>
-        </div>
-        
-    );
-  }
-  
-  export default Navbar;
-  
+          </li>
+          <li>
+            <div className="link">
+                <BsGraphUp className="icon"></BsGraphUp>
+                <div>Analytics</div>
+            </div>
+          </li>
+          <li>
+            <div className="link">
+                <BsFillPersonLinesFill className="icon"></BsFillPersonLinesFill>
+                <div>Employee</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

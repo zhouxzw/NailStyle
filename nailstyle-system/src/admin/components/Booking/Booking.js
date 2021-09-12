bdc081083a031642e12079290d46e6104ed69322
zhouxzw <44952147+zhouxzw@users.noreply.github.com>
2021-09-11 19:31:48 -0600

    Added react-calender component

--- nailstyle-system/src/admin/components/Booking/Booking.js
+++ nailstyle-system/src/admin/components/Booking/Booking.js
@@ -1,5 +1,25 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./Booking.css";
+
 const Booking = () => {
-  return (<div>Booking Page</div>);
+  const [date, onChange] = useState(new Date());
+
+  return (
+    <div className="booking-container">
+      <Calendar
+        className={"calender"}
+        minDate={new Date()}
+        prev2Label={null}
+        next2Label={null}
+        showFixedNumberOfWeeks={true}
+        onChange={onChange}
+        value={date}
+      />
+      {console.log("Date ", date)}
+    </div>
+  );
 };
 
 export default Booking;

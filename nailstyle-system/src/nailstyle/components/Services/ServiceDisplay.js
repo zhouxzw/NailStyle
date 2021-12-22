7b2783be401cb4f90eae63086b50aaa302917ace
zhouxzw <44952147+zhouxzw@users.noreply.github.com>
2021-12-21 22:37:29 -0700

    update

--- nailstyle-system/src/nailstyle/components/Services/ServiceDisplay.js
+++ nailstyle-system/src/nailstyle/components/Services/ServiceDisplay.js
@@ -3,6 +3,10 @@ import Service from "./Service";
 
 export default function ServiceDisplay({ servicesState }) {
   return servicesState.map((service, i) => {
-    if (service.toggled) return <Service service={service} key={"sd " + i} />;
+    if (service.toggled) {
+      return <Service service={service} key={"sd " + i} />;
+    } else {
+      return <></>;
+    }
   });
 }
